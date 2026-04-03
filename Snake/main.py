2b4189ba669b89eb8ca32b8e4ab3048d6fe1dbdb
Jean Stevenin <jean.stevenin@pimas.fr>
2026-04-03 13:30:43 +0200

    Ajout du score basique

--- Snake/main.py
+++ Snake/main.py
@@ -3,15 +3,21 @@ import time
 import random
 
 pygame.init()
-font = pygame.font.SysFont(None, 100)
+
+# Texte principale loose
+fontLoose = pygame.font.SysFont(None, 100)
+# Texte secondaire (plus petit)
+smallFontLoose = pygame.font.SysFont(None, 40)
+#Font du score
+scoreFont = pygame.font.SysFont(None, 40)
 
 #Flags
 game_over = False
 running = True
 
 #Constantes
-gridWidth = 10
-gridHeight = 10
+gridWidth = 15
+gridHeight = 15
 paddingGrid = 21
 screenSizeWidth = 750
 screenSizeHeight = 750
@@ -21,13 +27,13 @@ direction = {
     "DOWN": (1,0),
     "UP": (-1,0)
 }
-snakeSpeed = 500     # millisecondes entre chaque déplacement
+snakeSpeed = 200     # millisecondes entre chaque déplacement
 snakeDirection = ["RIGHT"] #Direction de départ
 snakeSizeStart = 4
 
-
+#Buffer
 lastMoveTime = 0
-
+scoreValue = 0
 
 snakePosition = []
 coordonneesGoal = None
@@ -38,7 +44,8 @@ grid = [[0 for x in range(gridHeight)] for y in range(gridWidth)] #[Y, X]
 screen = pygame.display.set_mode((screenSizeWidth, screenSizeHeight))
 clock = pygame.time.Clock()
 
-
+#SKIN
+#head_img = pygame.image.load("head.png").convert_alpha()
 
 def updateMap():
     for line in range(gridWidth):
@@ -70,7 +77,7 @@ def spawnSnake():
 
 
 def moveSnake():
-    global coordonneesGoal, grid ,snakeSize, game_over
+    global coordonneesGoal, grid ,snakeSize, game_over, scoreValue
     #grid = [[0 for x in range(gridWidth)] for y in range(gridHeight)]
 
     if len(snakeDirection) > 1:
@@ -90,10 +97,11 @@ def moveSnake():
             game_over = True
             
         snakePosition.append((nextLine, nextColumn)) #Y, X
-        if grid[nextLine][nextColumn] == 2:
+        if grid[nextLine][nextColumn] == 2: #On check si on a mangé un fruit
             coordonneesGoal = None
             snakeSize += 1
             snakePosition.insert(0, (previousLine, previousColumn))
+            scoreValue +=10
             #print(snakeSize)
     else: #Si collision
         game_over = True
@@ -124,13 +132,12 @@ def spawnGoal(): #Fait apparaitre l'objectif sur le terrain
 
 def printLoose():
     # Texte principal
-    text = font.render("PERDU", True, (255, 0, 0))
+    text = fontLoose.render("PERDU", True, (255, 0, 0))
     rect = text.get_rect(center=(screenSizeWidth/2, screenSizeHeight/2))
     screen.blit(text, rect)
 
-    # Texte secondaire (plus petit)
-    smallFont = pygame.font.SysFont(None, 40)
-    text2 = smallFont.render("Appuie sur SPACE pour recommencer", True, (255, 255, 255))
+
+    text2 = smallFontLoose.render("Appuie sur SPACE pour recommencer", True, (255, 255, 255))
 
     # Positionné EN DESSOUS du premier
     rect2 = text2.get_rect(center=(screenSizeWidth/2, rect.bottom + 30))
@@ -138,12 +145,13 @@ def printLoose():
     screen.blit(text2, rect2)
 
 def restartGame():
-    global grid, coordonneesGoal, snakeSize, snakeDirection
+    global grid, coordonneesGoal, snakeSize, snakeDirection, scoreValue
     snakePosition.clear()
     snakeSize = snakeSizeStart
     coordonneesGoal = None
     snakeDirection = ["RIGHT"]
     grid = [[0 for x in range(gridWidth)] for y in range(gridHeight)]
+    scoreValue = 0
     spawnSnake()
 
 
@@ -191,6 +199,13 @@ while running:
     for x in range(gridWidth):
         print(grid[x])
     """
+
+    # Texte principal
+    score = scoreFont.render(f"Score: {scoreValue:05} ", True, (255, 255, 255))
+
+    rect = score.get_rect(center=(screenSizeWidth/1.4, screenSizeHeight/16))
+    screen.blit(score, rect)
+
     pygame.display.flip()
     clock.tick(60)
 
